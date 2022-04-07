1b2addf336c401545a803c2e9296b58099ac2d1c
HHoaa <94822890+hoahoa191@users.noreply.github.com>
2022-04-07 17:04:36 +0700

    Update dataloader.py

--- modules/dataloader.py
+++ modules/dataloader.py
@@ -1,119 +1,56 @@
-import os
-from PIL import Image
+#@title tfrecord
 import torch
-from torch.utils import data
-import numpy as np
 from torchvision import transforms as T
-import torchvision
-import cv2
-import sys
+import tensorflow as tf
+"sourse : https://github.com/peteryuX/arcface-tf2/blob/6447e9d04451aba1ef262f43c77ae50cdfaf19f8/modules/dataset.py"
 
-
-class Dataset(data.Dataset):
-
-    def __init__(self, data_list_file, is_training=True, input_shape=(1, 112, 112)):
-        self.is_training = is_training
-        self.input_shape = input_shape
-        
-        with open(os.path.join(data_list_file), 'r') as fd:
-            imgs = fd.readlines()
-
-        imgs = [os.path.join(img.strip()) for img in imgs]
-        self.imgs = np.random.permutation(imgs)
-        self.size        = len(imgs)
-
-        normalize = T.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
-
-        if self.is_training:
-            self.transforms = T.Compose([
-                T.RandomCrop(self.input_shape[1:]),
-                T.ColorJitter(brightness=.4, hue=.3),
-                T.GaussianBlur(kernel_size=(5, 9), sigma=(0.1, 5)),
-                T.RandomHorizontalFlip(),
-                T.ToTensor(),
-                normalize
-            ])
+def _parse_tfrecord(binary_img=True):
+    def parse_tfrecord(tfrecord):
+        if binary_img:
+            features = {'image/source_id': tf.io.FixedLenFeature([], tf.int64),
+                        'image/encoded': tf.io.FixedLenFeature([], tf.string)}
+            x = tf.io.parse_single_example(tfrecord, features)
+            x_train = tf.image.decode_jpeg(x['image/encoded'], channels=3)
         else:
-            self.transforms = T.Compose([
-                T.CenterCrop(self.input_shape[1:]),
-                T.ToTensor(),
-                normalize
-            ])
+            features = {'image/source_id': tf.io.FixedLenFeature([], tf.int64),
+                        'image/img_path': tf.io.FixedLenFeature([], tf.string)}
+            x = tf.io.parse_single_example(tfrecord, features)
+            image_encoded = tf.io.read_file(x['image/img_path'])
+            x_train = tf.image.decode_jpeg(image_encoded, channels=3)
 
-    def __getitem__(self, index):
-        sample = self.imgs[index]
-        img_path = sample
-        #x
-        data = Image.open(img_path)
-        data = data.convert('RGB') # convert image to monochrome('L') or RGB
-        data = self.transforms(data)
-        #y
-        label = np.int32(sample.split("/")[-2])
-        return data.float(), label
+        y_train = tf.cast(x['image/source_id'], tf.int32)
 
-    def __len__(self):
-        return self.size
+        x_train = _transform_images()(x_train)
+        y_train = _transform_targets(y_train)
+        return x_train, y_train
+    return parse_tfrecord
 
-class LFWdataset(data.Dataset):
 
-    def __init__(self, data_list_file, input_shape=(1, 112, 112)):
-        self.input_shape = input_shape
-        
-        with open(os.path.join(data_list_file), 'r') as fd:
-            imgs = fd.readlines()
-        
-        imgs = [line.split() for line in imgs]
-        imgs = [[os.path.join(img[0].strip()), 
-                os.path.join(img[1].strip()),
-                int(img[2])] for img in imgs]
-                
-        self.imgs = np.random.permutation(imgs)
-        self.size        = len(imgs)
+def _transform_images():
+    def transform_images(x_train):
+        x_train = tf.image.random_flip_left_right(x_train)
+        x_train = x_train.numpy()
+        x_train = torch.from_numpy(x_train)
+        x_train = T.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))(x_train)
+        return x_train
+    return transform_images
 
-        normalize = T.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
-        self.transforms = T.Compose([
-                T.CenterCrop(self.input_shape[1:]),
-                T.ToTensor(),
-                normalize
-            ])
 
-    def __getitem__(self, index):
-        sample = self.imgs[index]
+def _transform_targets(y_train):
+    return torch.from_numpy(y_train.numpy())
 
-        #x1
-        data1 = Image.open(sample[0])
-        data1 = data1.convert('RGB') # convert image to monochrome('L') or RGB
-        data1 = self.transforms(data1)
-        #x2
-        data2 = Image.open(sample[1])
-        data2 = data2.convert('RGB') # convert image to monochrome('L') or RGB
-        data2 = self.transforms(data2)
-        #y
-        label = np.int32(sample[2])
-        return data1.float(), data2.float(), label
 
-    def __len__(self):
-        return self.size
-
-def get_DataLoader(dataset, batch_size, shuffle=True,num_workers=4):
-    return data.DataLoader(dataset, batch_size=batch_size,
-                        shuffle=shuffle,
-                        num_workers=num_workers)
-
-if __name__ == '__main__':
-    dataset = Dataset(data_list_file='/data/Datasets/fv/dataset_v1.1/mix_20w.txt',
-                      is_training=False,
-                      input_shape=(1, 112, 112))
-
-    trainloader = data.DataLoader(dataset, batch_size=10)
-    for i, (data, label) in enumerate(trainloader):
-        img = torchvision.utils.make_grid(data).numpy()
-        img = np.transpose(img, (1, 2, 0))
-        img += np.array([1, 1, 1]) #revert norm 
-        img *= 127.5
-        img = img.astype(np.uint8)
-        img = img[:, :, [2, 1, 0]]
-
-        cv2.imshow('img', img)
-        cv2.waitKey()
-        # break
+def load_tfrecord_dataset(tfrecord_path, batch_size,
+                          binary_img=True, shuffle=True, buffer_size=10240):
+    """load dataset from tfrecord"""
+    raw_dataset = tf.data.TFRecordDataset(tfrecord_path)
+    #raw_dataset = raw_dataset.repeat()
+    if shuffle:
+        raw_dataset = raw_dataset.shuffle(buffer_size=buffer_size)
+    dataset = raw_dataset.map(
+        _parse_tfrecord(binary_img=binary_img),
+        num_parallel_calls=tf.data.experimental.AUTOTUNE)
+    dataset = dataset.batch(batch_size)
+    dataset = dataset.prefetch(
+        buffer_size=tf.data.experimental.AUTOTUNE)
+    return dataset 
